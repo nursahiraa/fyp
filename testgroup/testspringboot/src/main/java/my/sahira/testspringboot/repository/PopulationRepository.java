ba38d69e23fdc0a15d29e5ae8e2c712284a450c7
Nur Sahira Zhamri <znursahira02@gmail.com>
2024-10-13 20:10:59 +0100

    Remove unnecessary deleted files

--- testgroup/testspringboot/src/main/java/my/sahira/testspringboot/repository/PopulationRepository.java
+++
@@ -1,8 +0,0 @@
-package my.sahira.testspringboot.repository;
-
-import my.sahira.testspringboot.model.Population;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PopulationRepository extends JpaRepository<Population, Long> {
-
-}
